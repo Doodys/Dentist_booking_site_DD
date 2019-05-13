e1dfbc652fd8a2debd4eea51e0eeedc0b810622a
Mike <dudys.michal@gmail.com>
2019-05-13 21:25:28 +0200

    Phone number input
    Added auto dash every third digit in phone number input

--- resources/js/enableClientID.js
+++ resources/js/enableClientID.js
@@ -5,12 +5,14 @@ function di_en() {
         document.getElementById("id_input_text").style.visibility = "visible";
         document.getElementById("full_form").style.visibility = "hidden";
         document.getElementById("form_part").required = false;
+        document.getElementById("txt").required = false;
         document.getElementById("id_input").required = true;
     } else if (sel == "TAK") {
         document.getElementById("id_input").style.visibility = "hidden";
         document.getElementById("id_input_text").style.visibility = "hidden";
         document.getElementById("full_form").style.visibility = "visible";
         document.getElementById("form_part").required = true;
+        document.getElementById("txt").required = true;
         document.getElementById("id_input").required = false;
     }
 };
